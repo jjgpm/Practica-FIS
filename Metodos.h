9aafc5579a798d81d95428f1c048bb251d8304e5
Sickcis <andres.arcones.crespo@gmail.com>
2017-03-10 12:45:57 +0100

    Add files via upload

---
+++ Metodos.h
@@ -0,0 +1,24 @@
+//
+//  Metodos.h
+//  Practica FIS
+//
+//  Created by Andrés Arcones Crespo on 10/3/17.
+//  Copyright © 2017 Andrés Arcones Crespo. All rights reserved.
+//
+
+#ifndef Metodos_h
+#define Metodos_h
+
+
+bool buscarPacientes(/*fichero, nombre*/);
+void agregarPaciente(/*fichero*/);
+void crearCalendario(Calendario.mes, Especialista);
+void mostrarCalendario(Calendario.mes, Especialista);
+int modificarCalendaria(Calendario.mes,Especialista): //parametros o dentro metodo
+
+
+
+
+
+
+#endif /* Metodos_h */
